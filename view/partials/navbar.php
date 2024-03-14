c0eaa02b15f951589d696e5e0eb898c3a2107122
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2024-03-14 10:31:53 +0100

    Add dropdown menus styles and fix focus state

--- view/partials/navbar.php
+++ view/partials/navbar.php
@@ -2,7 +2,7 @@
         <div class="wrapper">
             <div class="logo-container">
                 <a href="/">BGA</a>
-                <button type="button" id="toggle" aria-expanded="false" aria-haspopup="true" aria-controls="main-nav">
+                <button type="button" id="menu" aria-expanded="false" aria-haspopup="true" aria-controls="main-nav">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                         <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
                     </svg>
